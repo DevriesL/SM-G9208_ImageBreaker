e47fb23efa17e19c9835a8bd2d172c9947e18b1d
Devries <therkduan@gmail.com>
2016-01-13 22:40:00 +0800

    add build script

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+    set -x
+
+    export ARCH=arm64
+
+    export PATH=/home/devries/gcc/linux-x86/aarch64/aarch64-linux-android-4.9:$PATH
+
+    export PATH=/home/devries/GitHub/mkbootimg_tools:$PATH
+
+    export CROSS_COMPILE=/home/devries/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+
+    make exynos7420-zerofltetw_defconfig
+
+    make -j7
+
+mkbootimg \
+      --kernel arch/arm64/boot/Image \
+      --ramdisk ramdisk.packed \
+      --base 0x10000000 \
+      --pagesize 2048 \
+      --dt dt.img \
+      --ramdisk_offset 0x01000000 \
+      --tags_offset 0x00000100 \
+      --output boot.img
